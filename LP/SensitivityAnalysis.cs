d949bca7e66d93bb2bb33c115318e28f5df5f534
Marth1nus <tinus1.prinsloo@gmail.com>
2024-08-16 21:13:31 +0200

    Ranges for Basic variables

--- LP/SensitivityAnalysis.cs
+++ LP/SensitivityAnalysis.cs
@@ -11,8 +11,43 @@ namespace LPR381.LP
     {
         public static string Analise(Tableau tableau)
         {
+            var analysis = new StringBuilder();
 
-            return "";
+            var basicVariableIndices = tableau.GetBasicVariableIndices().ToArray();
+
+            var B = Matrix<double>.Build.DenseOfColumnArrays(
+                basicVariableIndices.Select(
+                    j => Enumerable.Range(1, tableau.Height).Select(
+                    i => tableau.InitialTable[i, j]).ToArray()).ToArray());
+            var BInverse = B.Inverse();
+
+            var Cbv = Vector<double>.Build.DenseOfEnumerable(
+                basicVariableIndices.Select(
+                    j => tableau.InitialTable[0, j]));
+
+            var shadowPrices = Cbv * BInverse;
+
+            for (int k = 0; k < basicVariableIndices.Length; k++)
+            {
+                var j = basicVariableIndices[k];
+                var Cb = Cbv[k];
+
+                var allowableIncrease = double.PositiveInfinity;
+                var allowableDecrease = double.PositiveInfinity;
+
+                for (int i = 1; i < tableau.Height; i++)
+                {
+                    var coefficient = tableau.InitialTable[i, j];
+                    if (coefficient > 0)
+                        allowableIncrease = Math.Min(allowableIncrease, shadowPrices[i - 1] / coefficient);
+                    else if (coefficient < 0)
+                        allowableDecrease = Math.Min(allowableDecrease, -shadowPrices[i - 1] / coefficient);
+                }
+
+                analysis.AppendLine($"C{tableau.ColumnNames[j]} can range from ({Cb} - {allowableDecrease} = {Cb - allowableDecrease}) to ({Cb} + {allowableIncrease} = {Cb + allowableDecrease})");
+            }
+
+            return analysis.ToString();
         }
     }
 }
