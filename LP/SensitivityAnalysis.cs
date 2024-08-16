cd543004475d59bd3545b354a740248e20a3e5c0
Marth1nus <tinus1.prinsloo@gmail.com>
2024-08-16 16:29:42 +0200

    Sensitivity Analysis Prototype

---
+++ LP/SensitivityAnalysis.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LPR381.LP
+{
+    public class SensitivityAnalysis
+    {
+        public static string Analise(Tableu tableu)
+        {
+            return "";
+        }
+    }
+}
