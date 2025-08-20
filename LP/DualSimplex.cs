8570371f547357b40342e9a09de391f8075e09b4
Marth1nus <tinus1.prinsloo@gmail.com>
2025-08-20 19:15:53 +0200

    (fix) Bad index check in DualSimplex.DualForm(tableau)

--- LP/DualSimplex.cs
+++ LP/DualSimplex.cs
@@ -54,20 +54,31 @@ namespace LPR381.LP
             return steps;
         }
 
-        public static (Tableau tableau, bool madeChanges) DualFrom(Tableau tableau, List<string> steps = null)
+        public static (Tableau tableau, bool madeChanges) DualFrom(Tableau tableau, List<string> steps)
         {
             bool madeChanges = false;
             for (int j = 0; j < tableau.Width; j++)
             {
-                int indexOfNegative1 = -1;
+                int Negative1I = -1;
                 // indentify basic-like column. remember i of -1
-                if (!Enumerable.Range(0, tableau.Height).Skip(1).Select(i => (v: tableau[i, j], i))
-                    .All(p => p.v == 0.0 || p.v == -1.0 && indexOfNegative1 == -1 && (indexOfNegative1 = p.i) != -1))
-                    continue;
+                for (int i = 1; i < tableau.Height; i++)
+                {
+                    if (tableau[i, j] == -1)
+                    {
+                        Negative1I = i;
+                    }
+                    else if (tableau[i, j] != 0)
+                    {
+                        Negative1I = -1; // non basic-like column
+                        break;
+                    }
+                }
+                if (Negative1I < 0)
+                    continue; // non basic-like column found
                 // Multiply the row by -1
                 for (int k = 0; k < tableau.Width; k++)
-                    tableau[indexOfNegative1, k] *= -1;
-                steps?.Add($"Multiplied row **{tableau.RowNames[indexOfNegative1]}** by -1\nTo make **{tableau.ColumnNames[j]}** basic\n{tableau}");
+                    tableau[Negative1I, k] *= -1;
+                steps.Add($"Multiplied row **{tableau.RowNames[Negative1I]}** by -1\nTo make **{tableau.ColumnNames[j]}** basic\n{tableau}");
                 madeChanges = true;
             }
             return (tableau, madeChanges);
