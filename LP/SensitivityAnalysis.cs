701888959ba5b8c8e662ac36454aeef934de5093
Marth1nus <tinus1.prinsloo@gmail.com>
2025-08-27 14:48:00 +0200

    (notes) Added requirements for sensitivity analysis as a comment

--- LP/SensitivityAnalysis.cs
+++ LP/SensitivityAnalysis.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Reflection;
+using System.Security.Policy;
 using System.Text;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
 
 namespace LPR381.LP
 {
@@ -9,7 +13,24 @@ namespace LPR381.LP
         public static List<String> Solve(Tableau tableau)
         {
             var steps = new List<String>() { "Start Sensitivity Analysis" };
-            // code here <3
+            /* 
+             * Display the range of a selected Non - Basic Variable.
+             * Apply and display a change of a selected Non - Basic Variable.
+             * Display the range of a selected Basic Variable.
+             * Apply and display a change of a selected Basic Variable.
+             * Display the range of a selected constraint right-hand - side value.
+             * Apply and display a change of a selected constraint right-hand - side value.
+             * Display the range of a selected variable in a Non-Basic Variable column.
+             * Apply and display a change of a selected variable in a Non-Basic Variable column.
+             * Add a new activity to an optimal solution.
+             * Add a new constraint to an optimal solution.
+             * Display the shadow prices.
+             * Duality:
+                 * Apply Duality to the programming model.
+                 * Solve the Dual Programming Model.
+                 * Verify whether the Programming Model has Strong or Weak Duality.
+            */
+            // Code here <3
             steps.Add("End Sensitivity Analysis");
             return steps;
         }
